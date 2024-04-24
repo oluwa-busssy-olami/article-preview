6ee1614570b195e4d2de14257e7d4d8303604111
Oluwanifesimi-Kenny <busolamikenny@gmail.com>
2024-04-24 07:41:51 +0100

    mobile screen

--- article-preview-component-master/index.js
+++ article-preview-component-master/index.js
@@ -1,2 +1,6 @@
 const socials_icon = document.querySelector(".shared");
-const share_button = document.querySelector(".share-button");
+const share_button = document.querySelector(".shareIcon");
+share_button.addEventListener("click", (e) => {
+  socials_icon.classList.add("clicked");
+  console.log("clicked");
+});

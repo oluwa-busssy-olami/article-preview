b5e7cb5888bc1c1d5d34780ff712022e9b41d7f6
Oluwanifesimi-Kenny <busolamikenny@gmail.com>
2024-04-24 16:08:12 +0100

    final change

--- article-preview-component-master/index.js
+++ article-preview-component-master/index.js
@@ -1,6 +1,12 @@
+//
+
 const socials_icon = document.querySelector(".shared");
 const share_button = document.querySelector(".shareIcon");
+const share_social_icon = document.querySelector(".share-social-icon");
 share_button.addEventListener("click", (e) => {
-  socials_icon.classList.add("clicked");
+  socials_icon.classList.toggle("clicked");
   console.log("clicked");
+});
+share_social_icon.addEventListener("click", (e) => {
+  socials_icon.classList.remove("clicked");
 });
